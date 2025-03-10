998c2c0a88c8fc6957a61fa312cfc54f785d1e53
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-03-10 14:54:00 +0100

    commenting out unnecessary packages

--- Package.swift
+++ Package.swift
@@ -6,9 +6,8 @@ import PackageDescription
 let package = Package(
     name: "numbers-parser",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
+        // .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.4"),
         .package(url: "https://github.com/leviouwendijk/plate.git", from: "1.0.2")
     ],
     targets: [
@@ -17,9 +16,8 @@ let package = Package(
         .executableTarget(
             name: "numbers-parser",
             dependencies: [
-                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                // .product(name: "SwiftProtobuf", package: "swift-protobuf"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "OrderedCollections", package: "swift-collections"),
                 .product(name: "plate", package: "plate"),
             ]
         )
